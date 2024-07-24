e02992e16aca644aea3d6a8c99a88ee0b89a53a5
Rafael <rafaelmoura7033@gmail.com>
2024-07-24 10:59:12 -0300

    Criando rota inicial de users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/users', function (Request $request) {
+    return response()->json([
+        'status' => true,
+        'message' => "Listar usuários",
+    ]);
+});
