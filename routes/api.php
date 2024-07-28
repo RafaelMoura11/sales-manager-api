28969573048ef5729fede00e7f96557ce7b50536
Rafael <rafaelmoura7033@gmail.com>
2024-07-28 15:28:29 -0300

    Adicionando autenticação nas demais rotas

--- routes/api.php
+++ routes/api.php
@@ -14,23 +14,27 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::put('/users', [UserController::class, 'update']);
     Route::delete('/users', [UserController::class, 'delete']);
     Route::post('/logout', [UserController::class, 'logout']);
+
+
+    Route::get('/customers', [CustomerController::class, 'index']);
+    Route::post('/customers', [CustomerController::class, 'store']);
+    Route::get('/customers/{id}', [CustomerController::class, 'show']);
+    Route::put('/customers/{id}', [CustomerController::class, 'update']);
+    Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
+    
+
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    
+    
+    Route::get('/sales', [SaleController::class, 'index']);
+    Route::post('/sales', [SaleController::class, 'store']);
+    Route::get('/sales/{id}', [SaleController::class, 'show']);
+    Route::put('/sales/{id}', [SaleController::class, 'update']);
+    Route::delete('/sales/{id}', [SaleController::class, 'destroy']);
 });
 
-Route::get('/customers', [CustomerController::class, 'index']);
-Route::post('/customers', [CustomerController::class, 'store']);
-Route::get('/customers/{id}', [CustomerController::class, 'show']);
-Route::put('/customers/{id}', [CustomerController::class, 'update']);
-Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-
-Route::get('/sales', [SaleController::class, 'index']);
-Route::post('/sales', [SaleController::class, 'store']);
-Route::get('/sales/{id}', [SaleController::class, 'show']);
-Route::put('/sales/{id}', [SaleController::class, 'update']);
-Route::delete('/sales/{id}', [SaleController::class, 'destroy']);
 
