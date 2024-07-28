d404c1d8c7a42f1ca31f6f5feffdec3a65676abe
Rafael <rafaelmoura7033@gmail.com>
2024-07-28 13:52:17 -0300

    Removendo import desnecessário

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
-use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
